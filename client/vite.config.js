324415136319d15d5d1efad37d3bc9db104ac187
elfox14 <86858355+elfox14@users.noreply.github.com>
2026-04-26 01:11:10 +0300

    Fix white screen by setting vite base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/fin/',
-  build: {
-    rollupOptions: {
-      output: {
-        // تعطيل الأرقام العشوائية في أسماء الملفات
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/fin/', // ضروري جداً لكي تعمل الروابط على mcprim.com/fin
 })
