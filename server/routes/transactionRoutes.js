6ea8fc001659c2a69bde36602b4fe269dd1a8782
elfox14 <86858355+elfox14@users.noreply.github.com>
2026-04-29 01:40:27 +0300

    Fix white screen by setting vite base path

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createTransaction, getTransactions, updateTransactionStatus } = require('../controllers/transactionController');
-const { protect } = require('../middlewares/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', protect, createTransaction);
 router.get('/', protect, getTransactions);
